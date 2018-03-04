d1427abb75bbf74022ce78ea8e1b9b4d6dd0d24a
Abbey Moore <abbeymoore@D-173-250-201-251.dhcp4.washington.edu>
2018-03-03 16:54:08 -0800

    fixed parenthesis

--- ui.R
+++ ui.R
@@ -5,30 +5,29 @@ my.ui <- (navbarPage("Hip-Hop Lyrics and Politicians",
                      tabPanel("Jarod"),
                      #jarod's sidepannels/inputs here
                      
-                     tabPanel("Abbey"),
+                     tabPanel("Abbey",
                      #abbey's sidepannels/inputs here
-                     # fluidPage(
-                     #   titlePannel("Word Cloud"),
-                     #   sidebarLayout(
-                     #     sidebarPannel(
-                     #       selectInput("politician", "Choose a politician:",
-                     #                   choices = c("Mike Huckabee", "Jeb Bush", "Ben Carson",
-                     #                               "Chris Christie", "Ted Cruz", "Hillary Clinton",
-                     #                               "Donald Trump")),
-                     #       sliderInput("freq",
-                     #                   "Minimum Frequency:",
-                     #                   min = 1, max = 10, value = 5),
-                     #       sliderInput("max",
-                     #                   "Maximum Number of Words:",
-                     #                   min = 1, max = 20, value = 10)
-                     #     ),
-                     #     mainPanel(
-                     #       plotOutput("wordPlot")
-                         # )
-                     #   )
-                     #   
-                     # ),
-                     # 
+                     fluidPage(
+                       titlePannel("Word Cloud"),
+                       sidebarLayout(
+                         sidebarPannel(
+                           selectInput("politician", "Choose a politician:",
+                                       choices = c("Mike Huckabee", "Jeb Bush", "Ben Carson",
+                                                   "Chris Christie", "Ted Cruz", "Hillary Clinton",
+                                                   "Donald Trump")),
+                           sliderInput("freq",
+                                       "Minimum Frequency:",
+                                       min = 1, max = 10, value = 5),
+                           sliderInput("max",
+                                       "Maximum Number of Words:",
+                                       min = 1, max = 20, value = 10)
+                         ),
+                         mainPanel(
+                           plotOutput("wordPlot")
+                         )
+                       )
+                     )
+                     )
                      tabPanel("Rasik",
                      fluidPage(
                        titlePanel("Mentions of 2016 Primary Candidates"),
