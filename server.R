5ceaf93ea5cd85bd4f4a2e41cc76a477a411a6e6
Rasik Parikh <rasikparikh@hotmail.com>
2018-03-03 17:46:17 -0800

    Update hover

--- server.R
+++ server.R
@@ -7,8 +7,16 @@ our.server <- function(input,output) {
   hip.hop.data <- read.csv("data/genius_hip_hop_lyrics.csv", stringsAsFactors = FALSE)
   
   output$mentions <- renderPlot({
-    primary.candidates <- hip.hop.data %>% 
-      filter()
+    ggplot(hip.hop.data, aes(album_release_date)) + 
+      geom_dotplot(aes(fill = candidate)) +
+      theme(legend.position = "top") +
+      labs(title = "Mentions of 2016 Primary Candidates", x = "Date", y = "Frequency") 
+  })
+  
+  output$lyric <- renderPrint({
+    lyric.info <- nearPoints(hip.hop.data, input$lyric)
+    req(nrow(brand.info) != 0)
+    print(lyric.info, row.names = FALSE)
   })
 }
 
